c2c38ffbb090ce675c8a7a0739a00ce68578d56e
Alan Collins <alan.collins@bath.edu>
2018-10-30 19:59:43 -0400

    Fixed dorectory for new structure

--- Download_Genomes.R
+++ Download_Genomes.R
@@ -1,3 +1,3 @@
 url <- "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt"
-dest <- "/Users/AlanCollins/Github/mapa-like-search/All_genomes_proks.txt" 
+dest <- "Data/All_genomes_proks.txt" 
 download.file(url, dest) 
