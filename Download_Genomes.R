d07b6d59fb702bc2ab033b0115df3cfe0fad68be
Alan Collins <alan.collins@bath.edu>
2018-11-06 16:08:50 -0500

    Updated raw file download information

--- Download_Genomes.R
+++ Download_Genomes.R
@@ -1,3 +1,16 @@
+#Genomes
+
 url <- "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt"
 dest <- "Data/All_genomes_proks.txt" 
 download.file(url, dest) 
+
+
+# LapD- like.
+# Retrieved from https://www.ncbi.nlm.nih.gov/sites/entrez?LinkName=cdd_protein&db=cdd&cmd=Link&from_uid=318615
+# on 10/31/2018
+# There are 13208 records
+
+# LapG- like.
+# Retrieved from https://www.ncbi.nlm.nih.gov/sites/entrez?LinkName=cdd_protein&db=cdd&cmd=Link&from_uid=310549
+# on 10/31/2018
+# There are 21140 records
